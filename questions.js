6951f7ce1d6150a1c70386bad008583f43bbcbb2
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-01-03 15:47:47 -0500

    questions added

---
+++ questions.js
@@ -0,0 +1,28 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables",
+        choices: ["commas", "curly brackets", "quotes", "paretheses"],
+        answer: "quotes"
+    },
+    {
+        title: "A very usefull tool used during development and debugging for printin content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log"
+    },   
+  ];
+  
