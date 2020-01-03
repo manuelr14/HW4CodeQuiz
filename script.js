f84729923deb6b91d60259999a2bacdd7b0ba597
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-01-03 17:04:17 -0500

    principal page structure begin

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 var timeEl = document.querySelector(".timer");
 var mainEl = document.getElementById("main");
+var tittleEl = document.querySelector(".tittle");
+var ScoreResultEl = document.querySelector(".ScoreResult");
 
+var secondsLeft = 5;
 
-var secondsLeft = 50;
+function principal(){
+
+    tittleEl.textContent = "Coding Quiz Challenge!";
+    mainEl.textContent = "Try to answer the following code-related questions withing the time limit. Keep in mind that incorret answer will penalize your score time by 10 seconds";
+
+}
 
 function setTime() {
   var timerInterval = setInterval(function() {
@@ -18,13 +26,13 @@ function setTime() {
 }
 
 function timeOut() {
+  mainEl.remove();  
   timeEl.textContent = "time:0" ;
+  tittleEl.textContent = "All Done!";
+  ScoreResultEl.textContent = "Your Score is:0"; 
 
-  var imgEl = document.createElement("img");
-
-  imgEl.setAttribute("src", "images/image_1.jpg");
-  mainEl.appendChild(imgEl);
+  
 
 }
-
+principal();
 setTime();
