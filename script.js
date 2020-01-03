3ab8c5ff62b63f153a671d6e238d68f4de345dcf
Manuel Ramirez <manuelramirez1411@gmail.com>
2020-01-03 17:28:36 -0500

    button principal page added

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ var timeEl = document.querySelector(".timer");
 var mainEl = document.getElementById("main");
 var tittleEl = document.querySelector(".tittle");
 var ScoreResultEl = document.querySelector(".ScoreResult");
-
+var buttonPlaceEL = document.querySelector("#buttonplace");
 var secondsLeft = 5;
 
 function principal(){
 
     tittleEl.textContent = "Coding Quiz Challenge!";
     mainEl.textContent = "Try to answer the following code-related questions withing the time limit. Keep in mind that incorret answer will penalize your score time by 10 seconds";
+    var btn = document.createElement("BUTTON");
+    btn.innerHTML = "Start Quiz";
+    buttonPlaceEL.appendChild(btn);
 
 }
 
@@ -27,6 +30,7 @@ function setTime() {
 
 function timeOut() {
   mainEl.remove();  
+  buttonPlaceEL.remove();
   timeEl.textContent = "time:0" ;
   tittleEl.textContent = "All Done!";
   ScoreResultEl.textContent = "Your Score is:0"; 
